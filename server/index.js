271a0c34291f90b4ef36a4aa21bc0fc3c5b9aecc
Satyam Pund <pundsatyam@gmail.com>
2023-04-14 14:07:09 +0530

    feat: server side razorpay setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Razorpay from 'razorpay';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+const instance = new Razorpay({ key_id: process.env.KEY_ID, key_secret: process.env.KEY_SECRET });
 
 mongoose.set('strictQuery', false);
 mongoose
@@ -14,6 +17,23 @@ mongoose
   .then(() => {
     console.log('connected to the database');
   });
+
+app.post('/order', async (req, res) => {
+  const { amount, notes } = req.body;
+
+  const order = await instance.orders.create({
+    amount: amount * 100,
+    currency: 'INR',
+    receipt: 'SATYAM_' + Math.floor(Math.random() * 10000),
+    notes: notes,
+  });
+
+  res.json({
+    success: true,
+    message: 'Order created successfully',
+    order: order,
+  });
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
